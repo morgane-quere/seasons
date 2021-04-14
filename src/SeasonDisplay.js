39e43f1f9521f3184aa9d6a6b68c5ff1222abf78
morgane quere <qu.morgane@gmail.com>
2021-04-14 09:57:47 +0200

    config object

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,5 +1,16 @@
 import React from 'react';
 
+const seasonConfig = {
+  winter: {
+    text: "Burr it's cold",
+    iconName: 'snowflake'
+  },
+  summer: {
+    text: "Let's hit the beach",
+    iconName: 'sun'
+  }
+}
+
 const getSeason = (lat, month) => {
   if (month > 2 && month < 9) {
     return lat > 0 ? 'summer' : 'winter';
@@ -10,11 +21,13 @@ const getSeason = (lat, month) => {
 
 const SeasonDisplay = (props) => {
   const season = getSeason(props.lat, new Date().getMonth());
-  const text = season === 'winter' ? "Burr it's chilly" : "Let's hit the beach"
+  const {text, iconName} = seasonConfig[season];
 
   return(
     <div>
+      <i className={`big ${iconName} icon`} />
       <h1>{text}</h1>
+      <i className={`big ${iconName} icon`} />
     </div>
   );
 };
